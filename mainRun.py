5dbd3a392ab4b74a69fd382d116dd35ca5501058
KarnGusain <rockypy17@gmail.com>
2017-08-29 15:20:44 +0530

    Update mainRun.py

--- mainRun.py
+++ mainRun.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+##################################################################################################################
+### Title: mainRun.py
+### Version: 02
+### Published : 30th March 2017
+### Author : Karn Kumar (karn.itguy@gmail.com)
+### This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+### without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+##################################################################################################################
 import processCheck
 import fsusaage
 import getSpeedInfo
