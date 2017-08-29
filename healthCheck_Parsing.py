6674f109e886e6b5a9fb887c9261441adf7140f6
KarnGusain <rockypy17@gmail.com>
2017-08-29 15:16:43 +0530

    Update healthCheck_Parsing.py

--- healthCheck_Parsing.py
+++ healthCheck_Parsing.py
@@ -1,4 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+##################################################################################################################
+### Title: healthCheck_Parsing.py
+### Version: 02
+### Published : 30th March 2017
+### Author : Karn Kumar (karn.itguy@gmail.com)
+### This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+### without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+##################################################################################################################
 import sys
 import re
 var1 = ''
